1ca14fe07a30a18f04df866dc82ac50148429cac
Patrick Brady <pj.brady@comcast.net>
2015-09-15 12:11:28 -0700

    This is a test of uploading power
    uploading power, go! :goat:

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -13,12 +13,14 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 		img = new Texture("download.jpg");
+		
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0.5f, 0.1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
