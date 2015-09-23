341d959ab387665fdd046077676bbfb1f83f052c
Patrick Brady <pj.brady@comcast.net>
2015-09-23 10:04:42 -0700

    Undoing the multitouch
    :haircut:

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -23,63 +23,27 @@ public class MyGdxGame extends ApplicationAdapter implements ApplicationListener
 	BitmapFont font;
 	Texture img, img2;
 	Sprite pew;
-	private String message = "Touch me!!";
 	float pewx = 50, pewy = 50;
 	float speed = 5;
-	
-	class TouchInfo {
-        public float touchX = 0;
-        public float touchY = 0;
-        public boolean touched = false;
-    }
-    
-    private Map<Integer,TouchInfo> touches = new HashMap<Integer,TouchInfo>();
-	
-	
+
 	@Override
 	public void create () {
-		Gdx.graphics.setDisplayMode(1720, 850, false);
+		Gdx.graphics.setDisplayMode(1000, 700, false);
 		batch = new SpriteBatch();
-		font = new BitmapFont();
-		//font = new BitmapFont(Gdx.files.internal("data/arial-15.fnt"),false);
-        font.setColor(Color.CYAN);
-		//font = new BitmapFont();
-		//font.setColor(Color.CYAN);
 		img2 = new Texture("download.jpg");
 		pew = new Sprite(img2);
 		
 		Gdx.input.setInputProcessor(this);
-		for(int i = 0; i < 5; i++){
-            touches.put(i, new TouchInfo());   
-		}
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(0.9f, 0.6f, 0.1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		pew.setPosition(pewx, pewy);
 		batch.begin();
 		pew.draw(batch);
-<<<<<<< HEAD
-		 message = "";
-	        for(int i = 0; i < 5; i++){
-	            if(touches.get(i).touched){
-	            	System.out.println(touches.get(i).touchX);
-	            	System.out.println(touches.get(i).touchY);
-	            	System.out.println(i);
-	            }                                
-	        }
-	        /*(TextBounds tb = font.getBounds(message);
-	        float x = w/2 - tb.width/2;
-	        float y = h/2 + tb.height/2;
-	        font.drawMultiLine(batch, message, x, y);*/
-		
-		//System.out.println("pewx:" + pewx);
-=======
->>>>>>> parent of ddceb9f... This has fully functioning cat fun.
-		//pew.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
 		batch.end();
 	}
 	
@@ -139,35 +103,19 @@ public class MyGdxGame extends ApplicationAdapter implements ApplicationListener
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		//pewx = screenX - pew.getWidth()/2;
-		//pewy = Gdx.graphics.getHeight() - screenY - pew.getHeight()/2;
-		if(pointer < 5){
-            touches.get(pointer).touchX = screenX;
-            touches.get(pointer).touchY = screenY;
-            touches.get(pointer).touched = true;
-        }
-		//System.out.println("pointer number touchdown is: " + pointer);
+		pewx = screenX - pew.getWidth()/2;
+		pewy = Gdx.graphics.getHeight() - screenY - pew.getHeight()/2;
 		return true;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		/*if(pointer < 5){
-            touches.get(pointer).touchX = screenX;
-            touches.get(pointer).touchY = screenY;
-            touches.get(pointer).touched = true;
-        }*/
         // TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		if(pointer < 5){
-            touches.get(pointer).touchX = screenX;
-            touches.get(pointer).touchY = screenY;
-            touches.get(pointer).touched = true;
-        }
 		pewx = screenX - pew.getWidth()/2;
 		pewy = Gdx.graphics.getHeight() - screenY - pew.getHeight()/2;
 		//pew.setPosition(screenX, screenY);
